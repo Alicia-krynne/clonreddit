52f03d98dc48994deb689ad4b6158f7b454e859e
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-07-15 11:51:18 +0300

    add urls.py

---
+++ appreddit/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView,LoginView
+
+app_name = "appreddit"
+
+urlpatterns=[
+    path('',views.homepage,name='Homepage'),
+    path('search/',views.search_results,name='search_results'),
+    path('new/image',views.create_post, name='create_post'),
+     
+ ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- redditclone/urls.py
+++ redditclone/urls.py
@@ -14,8 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls import url
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LogoutView,LoginView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('clone.urls')),
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/'),
+        name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('tinymce/', include('tinymce.urls')),
+    path('accounts/login/', LoginView.as_view(redirect_field_name ='/',success_url = '/'), name = 'login'),
+    path('logout/', LogoutView.as_view(), {"next_page": '/'}),
+   
 ]
